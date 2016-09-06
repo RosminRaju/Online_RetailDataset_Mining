f9aa85b6f38919758105c67aec5c7bee93b9eca7
WENYU ZHANG <wenyuzhang1992@gmail.com>
2016-09-05 21:50:32 -0400

    Delete files

--- Test_File.R
+++
@@ -1,6 +0,0 @@
-# This is a test file for Rstudio Github project
-# Counld be removed after testing
-# If you make any modification to the .r file or the project
-# Once you save the file 
-# Commit and push into Github, it will be seen by other co-workers
-data
